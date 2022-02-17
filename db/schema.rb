5429ce0c44b49bd5fa7b380d03ef1c31dcf5e178
muhme <github@heikol.de>
2022-02-17 06:10:57 +0100

    update Rails 7.0.2.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_164141) do
+ActiveRecord::Schema[6.1].define(version: 2022_01_29_164141) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
