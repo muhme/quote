75310e632ec6d56e4cf13e8c6936823d9cf3bdae
muhme <github@heikol.de>
2022-02-18 06:50:11 +0100

    enable docker container host name 'rails' for testing from headless Chrome

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,5 +72,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  # enable docker container host name
+  config.hosts << "rails"
 
 end
