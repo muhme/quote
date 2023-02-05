481705bf59d18404421f5cd5df764e5dcfbad28d
muhme <github@heikol.de>
2023-02-05 17:58:03 +0100

    adding tests for favicon and apple-touc-icon #3

--- test/system/layouts_test.rb
+++ test/system/layouts_test.rb
@@ -4,9 +4,20 @@ class LayoutsTest < ApplicationSystemTestCase
 
   test "favicon" do
     visit root_url
-    page.body.include? '<link rel="icon" type="image/x-icon" href='
-    # NICE to improve with something more intelligent, like the following line, but this one is not working
-    # page.has_xpath?("/html/head/link[@href='favicon.ico']", visible: false) 
+    assert page.source.match '<link rel="icon" type="image/x-icon" href="'
+    link = page.source.match /<link rel="icon" type="image\/x-icon" href="(.*)"/
+    assert_equal link.length, 2 # matching once?
+    assert_match /\/assets\/favicon\-.*/, link[1]
+    # TODO get favicon
   end
-  
+
+  test "apple touch icon" do
+    visit root_url
+    assert page.source.match '<link rel="apple-touch-icon"'
+    link = page.source.match /<link rel="apple-touch-icon" type="image\/png" href="(.*)"/
+    assert_equal link.length, 2 # matching once?
+    assert_match /\/assets\/apple\-touch\-icon\-.*/, link[1]
+    # TODO get apple-touch-icon
+  end
+
 end
