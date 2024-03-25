f449ce48cb84d071af7045e47103153dbc824527
muhme <github@heikol.de>
2024-03-25 16:46:00 +0100

    test coverage #61 quotations controller
    added some more tests and extended one test
    increased code coverage from 64% to 85% for quotations controller
    slow and steady wins the race

--- test/controllers/quotations_controller_test.rb
+++ test/controllers/quotations_controller_test.rb
@@ -141,6 +141,13 @@ class QuotationsControllerTest < ActionDispatch::IntegrationTest
       delete quotation_url id: @quotation_one
     end
     assert_forbidden
+    # try non-existing id
+    assert_no_difference 'Quotation.count' do
+      login :first_user
+      delete quotation_url id: 47114711
+      assert_response :not_found # 404
+      get '/logout'
+    end
     login :second_user
     assert_no_difference 'Quotation.count' do
       delete quotation_url @quotation_one
@@ -215,5 +222,126 @@ class QuotationsControllerTest < ActionDispatch::IntegrationTest
     assert_response :bad_request
     get '/quotations/list_no_public?page=42000000'
     assert_response :bad_request
+  end
+
+  test "author_selected - forbidden without login" do
+    # directly using the URL since named route helper isn't available
+    get "/en/quotations/author_selected/#{authors(:one).id}"
+    assert_forbidden
+  end
+
+  test "author selected - without locale, over all locales and with random author ids" do
+    login :first_user
+    locales = I18n.available_locales + [nil] # adding nil to test the scenario without a locale
+    locales.each do |locale|
+      random_author = Author.all.sample # select a random author from all authors
+      path = ""
+      path += "/#{locale}" if locale
+      path += "/quotations/author_selected/#{random_author.id}"
+      get path
+      assert_response :success
+      assert_equal "text/vnd.turbo-stream.html; charset=utf-8", @response.content_type
+      assert_match /turbo-stream action="update"/, @response.body
+      assert_match /<input type="search" name="author" id="author" value="/, @response.body
+    end
+  end
+
+  test "category_selected - forbidden without login" do
+    # directly using the URL since named route helper isn't available
+    get "/de/quotations/category_selected/#{categories(:one).id}"
+    assert_forbidden
+  end
+
+  # http://localhost:8102/de/quotations/category_selected/557,478,305-4,5
+  #
+  # <turbo-stream action="update" target="search_category_results"><template><turbo-frame id="search_category_results"></turbo-frame></template></turbo-stream><turbo-stream action="update" target="search_category_collected"><template><turbo-frame id="search_category_collected">
+  #   <input type="hidden" name="quotation[category_ids][]" id="quotation_category_ids_" value="557,478,305" autocomplete="off" />
+  #   <label class="font-size-smaller" for="category">3 Kategorien</label>
+  #     <script>
+  # //<![CDATA[
+  # myCleanField("category")
+  # //]]>
+  # </script>
+  #     <a class="animated" href="/de/quotations/delete_category/478,305-4,5">Bank<img alt="Rotes Kreuz" title="Löschen" border="0" src="/assets/cross-61afa45378baf5fbb81b0f0664d64291f9f51e22fad011a1ef99649cf5f3f3e3.png" /></a>
+  #     <a class="none" href="/de/quotations/delete_category/557,305-4,5">Alkohol<img alt="Rotes Kreuz" title="Löschen" border="0" src="/assets/cross-61afa45378baf5fbb81b0f0664d64291f9f51e22fad011a1ef99649cf5f3f3e3.png" /></a>
+  #     <a class="none" href="/de/quotations/delete_category/557,478-4,5">Ameise<img alt="Rotes Kreuz" title="Löschen" border="0" src="/assets/cross-61afa45378baf5fbb81b0f0664d64291f9f51e22fad011a1ef99649cf5f3f3e3.png" /></a>
+  #         <span class="example"><br /> mit einem Klick hinzufügen: </span>
+  #         <a href="/de/quotations/category_selected/4,557,478,305-4,5"><span class="example">China</span><img alt="Blitz" title="Hinzufügen" border="0" src="/assets/lightning-bf1d3f671d1761a63c677dbf9aeb9917d0d8bf3eae234937fe4b96c357e232cc.png" /></a>
+  #         <a href="/de/quotations/category_selected/5,557,478,305-4,5"><span class="example">Liebe</span><img alt="Blitz" title="Hinzufügen" border="0" src="/assets/lightning-bf1d3f671d1761a63c677dbf9aeb9917d0d8bf3eae234937fe4b96c357e232cc.png" /></a>
+  # </turbo-frame></template></turbo-stream>
+  #
+  test "category_selected - without locale, over all locales, selected and proposed ids" do
+    login :first_user
+    @categories = Category.all.sample(5)     # choose five categories
+    @selected_categories = @categories[0..2] # three of them as selected
+    @proposed_categories = @categories[3..4] # two of them as proposal
+    selected_ids = @selected_categories.map(&:id).join(',')
+    proposed_ids = @proposed_categories.map(&:id).join(',')
+    locales = I18n.available_locales + [nil] # adding nil to test the scenario without a locale
+    locales.each do |locale|
+      path = ""
+      path += "/#{locale}" if locale
+      path += "/quotations/category_selected/#{selected_ids}-#{proposed_ids}"
+      get path
+      assert_response :success
+      assert_equal "text/vnd.turbo-stream.html; charset=utf-8", @response.content_type
+      # check for the presence of the input with correct value
+      assert_select "input[name=?][value=?]", "quotation[category_ids][]", selected_ids
+      # verify links for adding each proposed category (in the beginning)
+      @proposed_categories.each do |category|
+        link = ""
+        link += locale ? "/#{locale}" : "/en" # locale defaults here to english if not set
+        link += "/quotations/category_selected/#{category.id},#{selected_ids}-#{proposed_ids}"
+        assert_select "a[href=?]", link
+        assert_select "a.animated", count: 1
+      end
+    end
+  end
+
+  test "delete_category - forbidden without login" do
+    # directly using the URL since named route helper isn't available
+    get "/quotations/delete_category/#{categories(:one).id}"
+    assert_forbidden
+  end
+
+  # http://localhost:8102/de/quotations/delete_category/557,478,305-4,5
+  #
+  #   <turbo-stream action="update" target="search_category_results"><template><turbo-frame id="search_category_results"></turbo-frame></template></turbo-stream><turbo-stream action="update" target="search_category_collected"><template><turbo-frame id="search_category_collected">
+  #   <input type="hidden" name="quotation[category_ids][]" id="quotation_category_ids_" value="557,478,305" autocomplete="off" />
+  #   <label class="font-size-smaller" for="category">3 Kategorien</label>
+  #     <a class="none" href="/de/quotations/delete_category/478,305-4,5">Bank<img alt="Rotes Kreuz" title="Löschen" border="0" src="/assets/cross-61afa45378baf5fbb81b0f0664d64291f9f51e22fad011a1ef99649cf5f3f3e3.png" /></a>
+  #     <a class="none" href="/de/quotations/delete_category/557,305-4,5">Alkohol<img alt="Rotes Kreuz" title="Löschen" border="0" src="/assets/cross-61afa45378baf5fbb81b0f0664d64291f9f51e22fad011a1ef99649cf5f3f3e3.png" /></a>
+  #     <a class="none" href="/de/quotations/delete_category/557,478-4,5">Ameise<img alt="Rotes Kreuz" title="Löschen" border="0" src="/assets/cross-61afa45378baf5fbb81b0f0664d64291f9f51e22fad011a1ef99649cf5f3f3e3.png" /></a>
+  #         <span class="example"><br /> mit einem Klick hinzufügen: </span>
+  #         <a href="/de/quotations/category_selected/4,557,478,305-4,5"><span class="example">China</span><img alt="Blitz" title="Hinzufügen" border="0" src="/assets/lightning-bf1d3f671d1761a63c677dbf9aeb9917d0d8bf3eae234937fe4b96c357e232cc.png" /></a>
+  #         <a href="/de/quotations/category_selected/5,557,478,305-4,5"><span class="example">Liebe</span><img alt="Blitz" title="Hinzufügen" border="0" src="/assets/lightning-bf1d3f671d1761a63c677dbf9aeb9917d0d8bf3eae234937fe4b96c357e232cc.png" /></a>
+  # </turbo-frame></template></turbo-stream>
+  #
+  test "delete_category - without locale, over all locales, selected and proposed ids" do
+    login :first_user
+    @categories = Category.all.sample(5)     # choose five categories
+    @selected_categories = @categories[0..2] # three of them as selected
+    @proposed_categories = @categories[3..4] # two of them as proposal
+    selected_ids = @selected_categories.map(&:id).join(',')
+    proposed_ids = @proposed_categories.map(&:id).join(',')
+    locales = I18n.available_locales + [nil] # adding nil to test the scenario without a locale
+    locales.each do |locale|
+      path = ""
+      path += "/#{locale}" if locale
+      path += "/quotations/delete_category/#{selected_ids}-#{proposed_ids}"
+      get path
+      assert_response :success
+      assert_equal "text/vnd.turbo-stream.html; charset=utf-8", @response.content_type
+      # check for the presence of the input with correct value
+      assert_select "input[name=?][value=?]", "quotation[category_ids][]", selected_ids
+      # verify links for adding each proposed category (in the beginning)
+      @proposed_categories.each do |category|
+        link = ""
+        link += locale ? "/#{locale}" : "/en" # locale defaults here to english if not set
+        link += "/quotations/category_selected/#{category.id},#{selected_ids}-#{proposed_ids}"
+        assert_select "a[href=?]", link
+        assert_select "a.animated", count: 0
+      end
+    end
   end
 end
