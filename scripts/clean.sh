479b306e427fa1dbdf84439c44eac8edd0c83164
muhme <github@heikol.de>
2024-06-20 07:24:54 +0200

    Also delete the Docker network with clean

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,7 +5,13 @@
 # MIT License, Copyright (c) 2007 - 2024 Heiko Lübbe
 # Ruby on Rail web application https://www.zitat-service.de, see https://github.com/muhme/quote
 
-PATTERN="^quote_[a-z]*$"
-echo ''
-echo "*** Removing all docker containers ${PATTERN}"
-docker ps -a --format '{{.Names}}' | grep "${PATTERN}" | xargs -r docker rm -f
+PREFIX="quote_"
+NETWORK_NAME="${PREFIX}default"
+
+echo '*** Remove following Docker containers'
+docker ps -a --format '{{.Names}}' | grep "^${PREFIX}" | xargs -r docker rm -f
+
+echo '*** Remove following Docker network'
+if docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
+  docker network rm "$NETWORK_NAME"
+fi
