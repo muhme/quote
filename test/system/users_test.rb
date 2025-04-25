00d81bddbfb071508a9ea9c31f21447147bf848d
muhme <github@heikol.de>
2025-04-25 08:54:13 +0200

    Fix one system test by using an existing image.
    As in current Chrome container the used image is no more existing, choosing another one.

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -201,8 +201,8 @@ class UsersTest < ApplicationSystemTestCase
     #   click_on 'Select file'
     #   attach_file('avatar-upload', '/tmp/face-smile.png', make_visible: true)
     # using hidden input field in bypassing Stimulus JS controller
-    # image file `face-smile.png` comes local from quote_chrome docker container
-    find('form input[type="file"]', visible: false).set('/usr/share/icons/Adwaita/48x48/legacy/face-smile.png')
+    # image file `google-chrome.png` comes local from quote_chrome docker container
+    find('form input[type="file"]', visible: false).set('/usr/share/icons/hicolor/256x256/apps/google-chrome.png')
     click_on 'Save'
     check_this_page page, nil, 'Your user entry "first_user" has been changed.'
   end
